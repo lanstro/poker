03436e5a9cbd690d36625e0d3fbbc32e7755c26e
Kai Fu <kaistuff@yahoo.com>
2014-01-02 15:37:42 +1100

    admins

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,9 @@
 module UsersHelper
-	def avatar_for(user)
-		image_tag("avatars/"+user.avatar.to_s+".png", alt: user.name+"'s avatar", class: "user_avatar")
+	def avatar_for(user, size)
+		if size=="small"
+			image_tag("avatars/"+user.avatar.to_s+"_small.png", alt: user.name+"'s avatar", class: "user_avatar")
+		else
+			image_tag("avatars/"+user.avatar.to_s+".png", alt: user.name+"'s avatar", class: "user_avatar")
+		end
 	end
 end
