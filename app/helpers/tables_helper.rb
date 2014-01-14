adad27ca8c53630a894e7895f06df7fd9dccce8e
Kai Fu <kaistuff@yahoo.com>
2014-01-14 14:03:36 +1100

    testing fix

--- app/helpers/tables_helper.rb
+++ app/helpers/tables_helper.rb
@@ -1,2 +1,15 @@
 module TablesHelper
+
+	def card_file_paths
+		paths = Dir.glob("app/assets/images/cards/*.png")
+		results = {}
+		paths.each do |a|
+			temp = File.basename(a, ".png")
+			results[temp]=image_path "cards/"+temp+".png"
+		end
+		lg = Logger.new("log/kai_log")
+		lg.info(results.inspect)
+		return results
+	end
+
 end
