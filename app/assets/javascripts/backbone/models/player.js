ccf81c2cf0447886ffcd2b021bda6691f4068e0f
Kai Fu <kaistuff@yahoo.com>
2014-01-16 14:06:00 +1100

    develop card view further

--- app/assets/javascripts/backbone/models/player.js
+++ app/assets/javascripts/backbone/models/player.js
@@ -2,10 +2,4 @@ var app = app || {};
 
 app.Player = Backbone.Model.extend ({
 	
-	update: function(info){
-		this.set('avatar', info.avatar);
-		this.set('balance', info.balance);
-		this.set('name', info.name);
-	}
-
 });
