cc74a593d83eaa10a0c88328ad22e7d4ebef2f27
lanstro <kaistuff@yahoo.com>
2014-01-24 23:56:34 +0000

    ready to start again

--- app/assets/javascripts/backbone/views/player.js
+++ app/assets/javascripts/backbone/views/player.js
@@ -11,9 +11,6 @@ app.PlayerView = Backbone.View.extend({
 			escape: 		 /\<\@\-(.+?)\@\>/gim
 		};
 		this.template= _.template($('#opponent_template').html());
-		this.handView= new app.SortedSubHandView({
-			model: this.model
-		});
 	},
 
 	render: function(){
@@ -21,6 +18,44 @@ app.PlayerView = Backbone.View.extend({
 		this.$el.html( this.template(val));
 		this.$el.append(this.handView.render().$el);
 		return this;
+	},
+	
+	render_hand: function(){
+		// this.$el.empty();
+
+		status= app.status();
+		if(status >= DEALING && status <= FOLDERS_NOTIFICATION){
+			this.$el.append($("#cards_back_template").html());
+			return this;
+		}
+		if(status >= SHOWING_DOWN_FRONT_NOTIFICATION && status <= FRONT_HAND_SUGAR){
+			index = FRONT_HAND;
+		}
+		else if (status >= SHOWING_DOWN_MID_NOTIFICATION && status <= MID_HAND_SUGAR){
+			index = MID_HAND;
+		}
+		else if (status >= SHOWING_DOWN_BACK_NOTIFICATION && status <= BACK_HAND_SUGAR){
+			index = BACK_HAND;
+		}
+		else {
+			return this;
+		}
+		_.each(this.model.get("arrangement")[index]["cards"], function(card){
+			console.log("trying to render "+JSON.stringify(card));
+			this.renderCard(card);
+		}, this);
+		return this;
+	},
+	
+	renderCard: function(card){
+		console.log("trying to render called for: "+JSON.stringify(card));
+		var cardView=new app.CardView({
+			model:card,
+		});
+		console.log("trying to render "+JSON.stringify(cardView));
+		this.$el.prepend(cardView.render().$el);
+	},
+	
 	}
 	
 });
--- app/assets/javascripts/backbone/views/sortedSubHand.js
+++ app/assets/javascripts/backbone/views/sortedSubHand.js
@@ -10,40 +10,5 @@ app.SortedSubHandView = Backbone.View.extend({
 
 	},
 	
-	render: function(){
-		this.$el.empty();
 
-		status= app.status();
-		if(status >= DEALING && status <= FOLDERS_NOTIFICATION){
-			this.$el.append($("#cards_back_template").html());
-			return this;
-		}
-		if(status >= SHOWING_DOWN_FRONT_NOTIFICATION && status <= FRONT_HAND_SUGAR){
-			index = FRONT_HAND;
-		}
-		else if (status >= SHOWING_DOWN_MID_NOTIFICATION && status <= MID_HAND_SUGAR){
-			index = MID_HAND;
-		}
-		else if (status >= SHOWING_DOWN_BACK_NOTIFICATION && status <= BACK_HAND_SUGAR){
-			index = BACK_HAND;
-		}
-		else {
-			return this;
-		}
-		_.each(this.model.get("arrangement")[index]["cards"], function(card){
-			console.log("trying to render "+JSON.stringify(card));
-			this.renderCard(card);
-		}, this);
-		return this;
-	},
-	
-	renderCard: function(card){
-		console.log("trying to render called for: "+JSON.stringify(card));
-		var cardView=new app.CardView({
-			model:card,
-		});
-		console.log("trying to render "+JSON.stringify(cardView));
-		this.$el.prepend(cardView.render().$el);
-	},
-	
 });
